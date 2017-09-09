ee4d17ecb00c7df4a0266640c7b6713e00cc77fb
lethe <lethe@mail.com>
2017-09-09 21:50:42 +0200

    Fixed resolve issue

--- node/test/request.js
+++ node/test/request.js
@@ -2,6 +2,6 @@ var vimlet_commons = require("../release");
 var path = require("path");
 
 var url = "https://nodejs.org/dist/v6.11.3/node-v6.11.3-x64.msi";
-var file = path.resolve("./resources/request/file");
+var file = path.join(__dirname, "resources/request/file");
 
 vimlet_commons.request.download(url, file);
