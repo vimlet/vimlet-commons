000025f07042e37f477383cca9f866b0c14625d9
lethevimlet <lethevimlet@mail.com>
2018-05-08 18:08:36 +0200

    WIP new structure

--- src/node/commons/index.js
+++
@@ -1,6 +0,0 @@
-exports.os = require("@vimlet/os");
-exports.run = require("@vimlet/run");
-exports.request = require("@vimlet/request");
-exports.compress = require("@vimlet/compress");
-exports.progress = require("@vimlet/progress");
-exports.io = require("@vimlet/io");
