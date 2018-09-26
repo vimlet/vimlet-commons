523435dd7fbd7f45e9c852eabf3c28d85dddccfe
jesusVimlet <jperefel@gmail.com>
2018-09-26 12:46:29 +0100

    Compress testing

--- test/node/compress/compress.js
+++ test/node/compress/compress.js
@@ -1,19 +1,27 @@
 var compress = require("../../../src/node/modules/compress");
+var io = require("../../../src/node/modules/io");
 var path = require("path");
+var fs = require("fs-extra");
 
 var pack = path.join(__dirname, "resources/compress/pack");
 var unpack = path.join(__dirname, "resources/compress/unpack");
-var out = path.join(__dirname, "resources/compress/compress");
+var out = path.join(__dirname, "resources/compress/compress.zip");
 
 
 compress.pack(pack, out, "zip", {
   doneHandler: function (error) {
     if (!error) {
+      console.log("Done Compress",io.absoluteFiles(io.getFiles(out)));
+      
+     
+
       compress.unpack(out, unpack, "zip", {
         doneHandler: function (error) {
-          console.log("Done");
+          console.log("Done upack", io.absoluteFiles(io.getFiles(unpack)));
         }
       });
+
+
     }
   }
 });
