7d8767f3990ed07b078868a900cd88cb77772c0a
jesusVimlet <jperefel@gmail.com>
2018-11-14 16:20:06 +0000

    Tests

--- test/node/io/commons-io.js
+++ test/node/io/commons-io.js
@@ -8,14 +8,14 @@ var exclude2 = "resources/test2/excluded/**/*";
 var includeFolder = "resources/test1";
 var includeFile = "resources/test1/js3.js.md";
 
-//var files = commons.io.getFiles([include,include2], [exclude,exclude2]);
+var files = commons.io.getFiles([include,include2], [exclude,exclude2]);
 //var files = commons.io.getFiles(include, exclude);
 // var files = commons.io.getFiles(includeFolder);
 //var files = commons.io.getFiles(includeFile);
 
 
 
-//console.log(files);
+console.log(files);
 
 
 console.log("Estoy en pattern", commons.io.isInPattern("resources/test1/folder2.js.md", "resources/test1/**/*.*"));
