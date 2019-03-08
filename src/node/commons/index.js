d87712841d31a4933a36129f6f8d23afb05128fe
jesusVimlet <jperefel@gmail.com>
2019-03-08 19:32:36 +0000

    Fixed dependencies

--- src/node/commons/index.js
+++ src/node/commons/index.js
@@ -1,14 +1,14 @@
 //@property os (public) {object} [Access to os lib]
-exports.os = require("@vimlet/os");
+exports.os = require("@vimlet/commons-os");
 //@property run (public) {object} [Access to run lib]
-exports.run = require("@vimlet/run");
+exports.run = require("@vimlet/commons-run");
 //@property request (public) {object} [Access to request lib]
-exports.request = require("@vimlet/request");
+exports.request = require("@vimlet/commons-request");
 //@property compress (public) {object} [Access to compress lib]
-exports.compress = require("@vimlet/compress");
+exports.compress = require("@vimlet/commons-compress");
 //@property progress (public) {object} [Access to progress lib]
-exports.progress = require("@vimlet/progress");
+exports.progress = require("@vimlet/commons-progress");
 //@property io (public) {object} [Access to io lib]
-exports.io = require("@vimlet/io");
+exports.io = require("@vimlet/commons-io");
 //@property copy (public) {object} [Access to copy lib]
-exports.copy = require("@vimlet/copy");
+exports.copy = require("@vimlet/commons-copy");
